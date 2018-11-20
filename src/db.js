03537cea0a6bef226a73c3af2e5b97ffccff5b01
nonki <thomas@flack.it>
2018-11-20 18:29:34 +0000

    lint that bitch

--- src/db.js
+++ src/db.js
@@ -3,12 +3,11 @@ const games = require('../config/local/games.json')
 module.exports = {
   getGameByKey: (keyId, cbk) => {
     const id = parseInt(keyId)
-    if (!games[id])
-      return cbk({error: 'Game not found.'}, null)
+    if (!games[id]) {return cbk({ error: 'Game not found.' }, null)}
 
     return cbk(null, games[id])
   },
   getGames: (limit, cbk) => {
     return cbk(null, games)
-  }
+  },
 }
--- src/index.js
+++ src/index.js
@@ -2,27 +2,22 @@ import 'dotenv/config'
 import express from 'express'
 import { getGameByKey, getGames } from './db.js'
 
-console.log('Loaded app');
+console.log('Loaded app')
 
 const app = express()
 
-app.get('/', (req, res) => {
-  res.set(200).send({
-    '/keys': {
-      type: 'collection',
-      children: {
-        '/keys/:keyId': {
-          type: 'singular',
-        }
-      }
-    }
+app.param('keyId', (req, res, next, id) => {
+  getGameByKey(id, (err, game) => {
+    if (err) {return res.set(404).send(err)}
+
+    req.game = game
+    next()
   })
 })
 
 app.get('/keys', (req, res) => {
   getGames(null, (err, games) => {
-    if (err)
-      return res.set(500).send(err)
+    if (err) {return res.set(500).send(err)}
 
     res.set(200).send(games)
   })
@@ -32,14 +27,8 @@ app.get('/keys/:keyId', (req, res) => {
   res.set(200).send(req.game)
 })
 
-app.param('keyId', (req, res, next, id) => {
-  getGameByKey(id, (err, game) => {
-    if (err)
-      return res.set(404).send(err)
-
-    req.game = game;
-    next();
-  });
+app.get('*', (req, res) => {
+  res.set(404).send()
 })
 
 app.listen(process.env.PORT || 8080)
