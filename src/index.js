14850549935c8b4817505d54657868e3fd7b1a6c
nonki <thomas@flack.it>
2018-11-22 15:06:42 +0000

    dumbass set set to status

--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
 import 'dotenv/config'
 import express from 'express'
 import { getGameByKey, getGames } from './db.js'
-
-console.log('Loaded app')
+import cors from 'cors';
 
 const app = express()
 app.use(express.json())
+app.use(cors());
 
 app.param('keyId', (req, res, next, id) => {
   getGameByKey(id, (err, game) => {
-    if (err) {return res.set(404).send(err)}
+    if (err) {return res.status(404).send(err)}
 
     req.game = game
     next()
@@ -17,36 +17,36 @@ app.param('keyId', (req, res, next, id) => {
 })
 
 app.get('/hongji', (req, res) => {
-  return res.set(200).send({text: "Wassup dog"})
+  return res.status(200).send({text: "Wassup dog"})
 })
 
 app.post('/auth', (req, res) => {
   if (!req.body.password) {
-    return res.set(400).send('HTTP 400 - Bad Request')
+    return res.status(400).send('HTTP 400 - Bad Request')
   }
 
   const password = req.body.password.toLowerCase()
   if (process.env.PASSWORD != password) {
-    return res.set(401).send('HTTP 401 - Unauthorized')
+    return res.status(401).send('HTTP 401 - Unauthorized')
   }
 
-  res.set(200).send('HTTP 200 - OK')
+  res.status(200).send('HTTP 200 - OK')
 })
 
 app.get('/keys', (req, res) => {
   getGames((err, games) => {
-    if (err) {return res.set(500).send(err)}
+    if (err) {return res.status(500).send(err)}
 
-    res.set(200).send(games)
+    res.status(200).send(games)
   })
 })
 
 app.get('/keys/:keyId', (req, res) => {
-  res.set(200).send(req.game)
+  res.status(200).send(req.game)
 })
 
 app.get('*', (req, res) => {
-  res.set(404).send('HTTP 404 - Not Found')
+  res.status(404).send('HTTP 404 - Not Found')
 })
 
 app.listen(process.env.PORT || 8080)
