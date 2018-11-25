ab8979ab9a049c5e2f811032d42e87efa74c98ea
nonki <thomas@flack.it>
2018-11-25 17:01:55 +0000

    easier handling of env

--- src/db.js
+++ src/db.js
@@ -1,16 +1,30 @@
-const games = require(`../config/local/games.${process.env.ENV}.json`)
+const games = require(`../config/local/games.json`)
 
 module.exports = {
   getGameByKey: (keyId, cbk) => {
     const id = parseInt(keyId)
-    const game = games.filter(el => parseInt(el.id) === id).pop()
+    let game = games.filter(el => parseInt(el.id) === id).pop()
     if (!game) {
       return cbk({}, null)
+    }
+
+    if (process.env.ENV.toLowerCase() !== 'live') {
+      game.key = '11111-22222-33333'
     }
 
     return cbk(null, game)
   },
   getGames: (cbk) => {
-    return cbk(null, games)
+    let gamesReturn = games
+    if (process.env.ENV.toLowerCase() !== 'live') {
+      gamesReturn = gamesReturn.map(el => {
+        return {
+          ...el,
+          key: '11111-22222-33333',
+        }
+      })
+    }
+
+    return cbk(null, gamesReturn)
   },
 }
